13613efd07d16a3669ac5983bd3bf3e1c80543bb
Luciano Martinez <luchofmartinez@gmail.com>
2026-02-16 19:44:34 -0300

    style: rediseño de seccion nosotros a estilo sobrio y profesional

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { Hero } from "./components/hero/hero";
 import { Projects } from "./components/projects/projects";
 import { Contact } from "./components/contact/contact";
 import { Header } from "./components/header/header";
+import { About } from "./components/about/about";
 
 @Component({
   selector: 'app-root',
-  imports: [Hero, Projects, Contact, Header],
+  imports: [Hero, Projects, Contact, Header, About],
   
   templateUrl: './app.html',
   styleUrl: './app.css'
