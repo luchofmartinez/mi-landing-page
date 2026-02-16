2bb4c1706d68104f1cb3453d84cf9afe408c75fc
Luciano Martinez <luchofmartinez@gmail.com>
2026-02-16 18:46:07 -0300

    feat: landing completa con secciones de proyectos y contacto

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Hero } from "./components/hero/hero";
+import { Projects } from "./components/projects/projects";
+import { Contact } from "./components/contact/contact";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Hero],
+  imports: [RouterOutlet, Hero, Projects, Contact],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
